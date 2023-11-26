35a006d5b456f2d9443ae686343fa340dfcef547
hwangheetae <tejaia@naver.com>
2023-11-26 22:51:52 +0900

    Squashed 'React/react-Course-record/' content from commit f88dc04
    git-subtree-dir: React/react-Course-record
    git-subtree-split: f88dc043f38227441bef941236cb932b269fe884

---
+++ README.md
@@ -0,0 +1,8 @@
+# React-Mini-blog
+
+인프런(https://www.inflearn.com/course/%EC%B2%98%EC%9D%8C-%EB%A7%8C%EB%82%9C-%EB%A6%AC%EC%95%A1%ED%8A%B8)
+
+처음 만나는 리액트 강의 수강기록입니다!
+
+## 참조
+https://github.com/soaple/mini-blog
---
+++ src/chapter_05/CommentList.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Comment from "./Comment";
+
+const comments= [
+    {
+        name:"이인제",
+        comment: "안녕하세요, 소플입니다."
+    },
+    {
+        name:"유재석",
+        comment: "리액트 재밌어요."
+    },
+    {
+        name:"Musk",
+        comment: "hi React."
+    }
+]
+function CommmentList(props){
+    return (
+        <div>
+            {comments.map((comment)=>{
+                return(
+                    <Comment name ={comment.name} comment={comment.comment} />
+                );
+
+            })}
+        </div>
+    );
+}
+
+export default CommmentList
