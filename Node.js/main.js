9802280c59fcbde850de7b09ee3f7db0eba15ac5
hwangheetae <tejaia@naver.com>
2024-01-04 20:51:56 +0900

    FINISH

--- Node.js/main.js
+++ Node.js/main.js
@@ -5,7 +5,6 @@ var qs = require("querystring");
 var template = require("./lib/template.js");
 var path = require("path");
 var sanitizeHtml = require("sanitize-html");
-
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
